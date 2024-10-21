34ba31b2aa9f2feac591462057245062b2fc09f5
LizMek <lizaassefa@gmail.com>
2024-10-21 10:03:28 -0400

    updated codes for backend

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -1,11 +1,16 @@
 const mysql2 = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const dbconnection = mysql2.createPool({
-  user: process.env.USER,
+  user: process.env.DBUSER,
   database: process.env.DATABASE,
-  host: process.env.HOST,
-  password: process.env.PASSWORD,
-  connectionLimit: 10,
+  host : process.env.HOST,
+  password: process.env.DBPASSWORD,
+  connectionLimit: 5,
+  port: 8889,
 });
+//const result =  dbconnection.execute("select 'test' ");
 
+// console.log("database connection established!") + result;
 module.exports = dbconnection.promise();
